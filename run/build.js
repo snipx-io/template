72041b7962065596a53e2d05f3ab3422a8ec6e41
arakilian0 <arakilian0@gmail.com>
2023-04-18 22:04:06 -0700

    CREATE webpack bundle scripts and update build.js

--- run/build.js
+++ run/build.js
@@ -1,3 +1,13 @@
 import chalk from 'chalk'
+import compiler from './scripts/webpack.js'
+
+
+compiler.run((err, stats) => {
+    // ...
+  
+    compiler.close((closeErr) => {
+      // ...
+    });
+  });
 
 console.log(chalk.red('run build'))
---
+++ run/scripts/webpack.js
@@ -0,0 +1,6 @@
+import webpack from 'webpack'
+import webpackConfig from '../../webpack.config.js'
+
+const compiler = webpack(webpackConfig)
+
+export default compiler
