103558d46ce1765ede2a3ce234b1bc671b57e6d6
arakilian0 <arakilian0@gmail.com>
2023-04-20 11:24:47 -0700

    ADD log.js, highlights input text and console log

---
+++ run/scripts/log.js
@@ -0,0 +1,29 @@
+import chalk from 'chalk' 
+
+const start = chalk.bold.cyan
+const error = chalk.bold.red
+const warning = chalk.bold.yellow
+const success = chalk.bold.green
+
+export default {
+    start: function (message) {
+        console.log(start('🔧 ' + message))
+    },
+    error: function (message) {
+        console.log(error('❌ ' + message))
+    },
+    warning: function (message) {
+        console.log(warning('⚠️ ' + message))
+    },
+    success: function (message) {
+        console.log(success('✅ ' + message))
+    },
+    end: {
+        pass: function (message) {
+            console.log(success('🚀 ' + message))
+        }, 
+        fail: function (message) {
+            console.log(error('🛑 ' + message))
+        }
+    }
+}
