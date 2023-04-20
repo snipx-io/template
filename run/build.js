738e6968e6941d61b42aeb6eae300918c6eded77
arakilian0 <arakilian0@gmail.com>
2023-04-20 11:43:18 -0700

    UPDATE webpack script, choose from dev or prod

--- run/build.js
+++ run/build.js
@@ -1,8 +1,8 @@
-import runWebpack from './scripts/webpack.js'
+import { production } from './scripts/webpack.js'
 import manifest from './scripts/manifest.js'
 import log from './scripts/log.js'
 
-let compiler = runWebpack.production()
+let compiler = production()
 
 compiler.run((err, stats) => {
     // ...
--- run/scripts/webpack.js
+++ run/scripts/webpack.js
@@ -2,11 +2,11 @@ import webpack from 'webpack'
 import webpackConfigDev from '../../config/webpack/development.js'
 import webpackConfigProd from '../../config/webpack/production.js'
 
-export default {
-    development: function () {
-        return webpack(webpackConfigDev)
-    },
-    production: function () {
-        return webpack(webpackConfigProd)
-    }
+export function development () {
+    return webpack(webpackConfigDev)
 }
+
+export function production () {
+    return webpack(webpackConfigProd)
+}
+
