e43f185306564e2d13e1faf1811c9655682cd31c
arakilian0 <arakilian0@gmail.com>
2023-04-20 10:24:07 -0700

    UPDATE manifest builder, now uses json function

--- run/scripts/manifest.js
+++ run/scripts/manifest.js
@@ -1,21 +1,52 @@
 import fs from 'fs'
 import path from 'path'
+import json from './json.js'
+import merge from 'deepmerge'
 
-let manifest = JSON.parse(fs.readFileSync(
-    path.resolve(`manifest.json`)))
+let manifestCommon, 
+    manifestTarget, 
+    manifestOutDir = path.resolve('out', 'build'),
+    manifestConfigDir = path.resolve('config', 'manifest')
 
-export default function (mode) {
+export default function (mode='development') {
+    // Check for valid argument
+    if (mode !== 'development' && mode !== 'production') {
+        mode = 'development'
+    }
+
+    // Create output folder
     if (!fs.existsSync(path.resolve('out'))) {
         fs.mkdirSync(path.resolve('out'))
+        // Create output build folder
+        if (!fs.existsSync(manifestOutDir)) {
+            fs.mkdirSync(manifestOutDir)
+        }
     }
-    if (!fs.existsSync(path.resolve('out', 'build'))) {
-        fs.mkdirSync(path.resolve('out', 'build'))
-    }
-
-    fs.writeFileSync(
-        path.resolve('out', 'build', 'manifest.json'), 
-        JSON.stringify(manifest, null, 2)
-    )
     
+    // Assign common manifest data
+    manifestCommon = json.read(
+        path.join(manifestConfigDir, 'common.json')
+    )
+
+    // Assign target manifest data
+    switch (mode) {
+        case 'development':
+            manifestTarget = json.read(
+                path.join(manifestConfigDir, `${mode}.json`)
+            )
+            break;
+        case 'production':
+            manifestTarget = json.read(
+                path.join(manifestConfigDir, `${mode}.json`)
+            )
+            break;
+    }
+    
+    // Write the manifest.json file
+    json.write(
+        path.join(manifestOutDir, 'manifest.json'), 
+        merge(manifestCommon, manifestTarget)
+    )
+
     return true
 }
