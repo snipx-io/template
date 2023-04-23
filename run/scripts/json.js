5b2e9fe7386161fb5bd684003599eee7cb22b896
arakilian0 <arakilian0@gmail.com>
2023-04-23 09:13:18 -0700

    UPDATE Write method into try/catch block

--- run/scripts/json.js
+++ run/scripts/json.js
@@ -1,4 +1,5 @@
 import fs from 'fs'
+import log from './log.js'
 
 export default {
     read: function (path) {
@@ -8,9 +9,13 @@ export default {
         return JSON.parse(fs.readFileSync(path))
     },
     write: function (path, data) {
-        fs.writeFileSync(
-            path, JSON.stringify(data, null, 2)
-        )
-        console.log('wrote manifest file to ' + path)
+        try {
+            fs.writeFileSync(
+                path, JSON.stringify(data, null, 2)
+            )
+            log.success('manifest file built successfully')
+        } catch(err) {
+            log.error('manifest file did not build successfully')
+        }
     }
 }
