165d91123e4f61a8ae0120ca2025f1d77f925032
arakilian0 <arakilian0@gmail.com>
2023-04-23 09:14:49 -0700

    CREATE development script, with webpack watcher

--- run/dev.js
+++ run/dev.js
@@ -1,3 +1,28 @@
-import chalk from 'chalk'
+import { development } from './scripts/webpack.js'
+import manifest from './scripts/manifest.js'
+import log from './scripts/log.js'
 
-console.log(chalk.red('run dev'))
+let compiler = development()
+
+log.start('initializing development mode')
+
+compiler.watch(
+    {
+        // Example
+        aggregateTimeout: 300,
+        poll: undefined,
+    },
+    (err, stats) => {
+        // Print watch/build result here...
+        if (!err) {
+            log.success('bundled development mode')
+            manifest('development')
+            log.end.pass('ready to try out in browser!')
+            console.log('================================')
+        }
+        else {
+            log.end.fail('bundle failed, read below to find out why:')
+            console.log(err)
+        }
+    }
+)
