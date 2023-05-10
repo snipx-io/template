43f6643ca3d73914ac53f7c31083f8bc8bfc82a8
arakilian0 <arakilian0@gmail.com>
2023-05-09 17:29:13 -0700

    MOVE eslint and prettier config files to root

--- run/lint.js
+++ run/lint.js
@@ -3,11 +3,11 @@ import { log, lintlog } from './scripts/log.js';
 
 if (process.env.npm_config_fix) {
   log.start(lintlog.startFix);
-  execSync('eslint -c ./config/.eslintrc.json . --fix', { stdio: 'inherit' });
+  execSync('prettier --write .', { stdio: 'inherit' });
   log.passed(lintlog.fixed);
 } else {
   log.start(lintlog.startNoFix);
-  execSync('eslint -c ./config/.eslintrc.json .', { stdio: 'inherit' });
+  execSync('eslint .', { stdio: 'inherit' });
 }
 
 log.passed(lintlog.end);
