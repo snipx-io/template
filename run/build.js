74488634ba4ed8305f8f0e276fcfdb9e19d71658
arakilian0 <arakilian0@gmail.com>
2023-06-20 14:08:44 -0700

    UPDATE build script using new path.js exports

--- run/build.js
+++ run/build.js
@@ -2,23 +2,33 @@ import webpack from 'webpack'
 import prodConfig from '../@webpack/webpack.prod.js'
 
 import manifest from '../lib/manifest.js'
-import zip from '../lib/zip.js'
 import { log, plog } from '../lib/log.js'
 
 const compiler = webpack(prodConfig)
 
 log.start(plog.start)
 
-compiler.run(err => {
-	if (err) console.log(err) // eslint-disable-line no-console
-	compiler.close(closeErr => {
-		if (!closeErr) {
-			log.passed(plog.bundle[1])
-			manifest('production')
-			if (process.env.npm_config_zip) zip()
-		} else {
-			log.failed(plog.bundle[0])
-			console.log(closeErr) // eslint-disable-line no-console
-		}
+function runWebpackCompiler (buildManifest) {
+	compiler.run(err => {
+		if (err) console.log(err) // eslint-disable-line no-console
+		compiler.close(closeErr => {
+			if (!closeErr) {
+				log.passed(plog.bundle[1])
+				if(buildManifest === true) manifest('production')
+			} else {
+				log.failed(plog.bundle[0])
+				console.log(closeErr) // eslint-disable-line no-console
+			}
+		})
 	})
-})
+}
+
+if (process.env.npm_config_W || process.env.npm_config_webpack) {
+	runWebpackCompiler(false)
+}
+else if (process.env.npm_config_M || process.env.npm_config_manifest) {
+	manifest('production')
+}
+else {
+	runWebpackCompiler(true)
+}
