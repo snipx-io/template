b969a1cdd0b6eb267ceba8f55af0f9d591cc33a0
arakilian0 <arakilian0@gmail.com>
2023-06-24 13:09:18 -0700

    UPDATE data/path.js (simplified)

--- data/path.js
+++ data/path.js
@@ -1,50 +1,10 @@
-import { resolve, join } from 'path'
-
-const _src = 'src'
-const _app = 'App.jsx'
-const _styles = 'styles'
-
-const _build = 'build'
-const _dist = 'dist'
-
-const _manifest = '@manifest'
-const _manifestDev = 'development.json'
-const _manifestProd = 'production.json'
-const _manifestCommon = 'common.json'
+import { resolve } from 'path'
 
 export default {
-    // src/App.jsx
-    app: join(resolve(_src), _app),
-    // src/styles/
-    styles: join(resolve(_src), _styles),
-    // build/
-    build: resolve(_build),
-    // dist/
-    dist: resolve(_dist),
-
-    // Takes an argument to return a specific manifest path.
-    manifest: (target) => {
-        // Set default path for manifest files.
-        let manifestPath = resolve(_manifest)
-        // Return best option for given target.
-        switch (target) {
-            // build/manifest.json
-            case 'build': 
-                return join(resolve(_build), 'manifest.json')
-            // @manifest/common.json
-            case 'common': 
-                return join(manifestPath, _manifestCommon)
-            // @manifest/development.json
-            case 'dev': 
-            case 'development': 
-                return join(manifestPath, _manifestDev)
-            // @manifest/production.json
-            case 'prod': 
-            case 'production': 
-                return join(manifestPath, _manifestProd)
-            // @manifest/
-            default: 
-                return manifestPath
-        }
-    }
+    webpack_entry: resolve('src/App.jsx'),
+    webpack_styles: resolve('src/styles'),
+    manifest_entry: resolve('manifest.json'),
+    manifest_output: resolve('build/manifest.json'),
+    output_zip: resolve('dist'),
+    output: resolve('build')
 }
