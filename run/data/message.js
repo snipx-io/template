2fb4c4c9ed2e33bc594493319d622fc870494925
arakilian0 <arakilian0@gmail.com>
2023-07-10 19:50:18 -0700

    UPDATE message data structure

--- run/data/message.js
+++ run/data/message.js
@@ -1,36 +1,23 @@
 export default {
-    production: {
-        start: 'initializing production bundle\n',
-        webpack: [
-            'webpack failed in production mode', // fail
-            'webpacked production mode', // pass
-        ],
-        manifest: [
-            'manifest production mode failed', // fail
-            'wrote manifest production mode', // pass
-        ],
-        zip: [
-            'production mode zip failed', // fail
-            'zipped in production mode', // pass
-        ],
-        end: 'all systems operational... \n'
-    },
-    development: {
-        start: 'initializing development bundle\n',
-        webpack: [
-            'webpack failed in development mode', // fail
-            'webpacked development mode', // pass
-        ],
-        manifest: [
-            'manifest development mode failed', // fail
-            'wrote manifest development mode', // pass
-        ],
-        end: 'watching for next change... \n================================'
-    },
+		webpack: {
+			start: 'initializing webpack in $ mode',
+			error: 'webpack encountered an error in $ mode',
+			success: 'webpack successfully bundled files in $ mode'
+		},
+		manifest: {
+			start: 'initializing manifest in $ mode',
+			error: 'manifest encountered an error in $ mode',
+			success: 'manifest successfully written in $ mode'
+		},
+		zip: {
+			start: 'initializing zip in $ mode',
+			error: 'zip encountered an error in $ mode',
+			success: 'zip successfully bundled files in $ mode'
+		},
     lint: {
-        startNoFix: 'initializing linter without --fix\n',
-        startFix: 'initializing linter with --fix\n',
-        fixed: 'fixed all rebel .js files',
-        end: 'ready to be committed'
-    }
+			start: 'initializing linter without --fix',
+			startFix: 'initializing linter with --fix',
+			error: 'linter encountered an error',
+			success: 'linter successfully formatted all files'
+		}
 }
